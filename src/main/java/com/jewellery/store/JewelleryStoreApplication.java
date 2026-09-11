5c597b9a6d5b9b0864fca9bed0f6f0dd5008a1e2
Dhruthi Spoorthi <dhruthispoorthi25@gmail.com>
2026-09-11 14:35:46 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/jewellery/store/JewelleryStoreApplication.java
@@ -0,0 +1,13 @@
+package com.jewellery.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JewelleryStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JewelleryStoreApplication.class, args);
+	}
+
+}
